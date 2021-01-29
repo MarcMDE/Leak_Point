4baae59ff8b1070afb208f1ef769bb6ed56ddb75
javi <jherreiz20@gmail.com>
2021-01-29 20:21:34 +0100

    JAVI

---
+++ src/GGJ21_Unity/Assets/Scripts/charMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charMove : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    private Vector3 speed;
+    private bool moving = false;
+    public float thrust = 1.0f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("space") && !moving){
+            rb.AddForce(transform.forward * thrust* 100* Time.deltaTime);
+            moving = true;
+            speed = transform.forward;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if (Input.GetKey("space")){
+
+            rb.velocity = Vector3.zero;
+            moving = false;
+        }
+
+    }
+}
