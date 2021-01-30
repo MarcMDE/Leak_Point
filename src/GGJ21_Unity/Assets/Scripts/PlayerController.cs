c1c02888b8266de0ed3fb46f873b5a05b949d0ca
marcmde <marcmde95@gmail.com>
2021-01-30 10:55:19 +0100

    diss mati

--- src/GGJ21_Unity/Assets/Scripts/PlayerController.cs
+++ src/GGJ21_Unity/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Rigidbody rb;
     private bool moving = false;
     [SerializeField] float thrust = 1.0f;
+    [SerializeField] float thrustSlowDownFactor = 0.1f;
     
     
     // Start is called before the first frame update
@@ -19,13 +20,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp("space") && rb.velocity == Vector3.zero)
+        if (Input.GetKeyUp("space") && !moving)
         {
-            rb.AddForce(Camera.main.transform.forward * thrust);
-            //moving = true;
+            Vector3 thrustDirection = Camera.main.transform.forward;
+            rb.AddForce( thrustDirection * thrust);
+            moving = true;
         }
         
         //if (rb.velocity == 0)
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb.velocity == Vector3.zero)
+            moving = false;
+
+        if (Mathf.Abs(rb.velocity.x) > thrustSlowDownFactor * Time.deltaTime || Mathf.Abs(rb.velocity.z) > thrustSlowDownFactor * Time.deltaTime)
+        {
+            Vector3 slowDirection = -rb.velocity.normalized;
+            slowDirection.y = 0f;
+            rb.AddForce(slowDirection * thrustSlowDownFactor * thrust * Time.deltaTime);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+        }
     }
 
     private void OnCollisionStay(Collision collision){
