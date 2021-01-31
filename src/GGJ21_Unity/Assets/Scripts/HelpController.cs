a0a88b009e9f28eeb36627a7c8e119e341f06c4f
javi <jherreiz20@gmail.com>
2021-01-31 09:52:06 +0100

    final?

---
+++ src/GGJ21_Unity/Assets/Scripts/HelpController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject reticle, help;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            help.SetActive(!help.activeInHierarchy);
+            reticle.SetActive(!reticle.activeInHierarchy);
+        }
+    }
+}
